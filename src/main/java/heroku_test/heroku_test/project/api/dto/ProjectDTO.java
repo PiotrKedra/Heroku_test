b89d81855eaae3e69c10725fc1fdeed2ce998c2f
piotrkedra <piotr.k9750@gmail.com>
2020-11-01 00:16:49 +0100

    primary constructor error

--- src/main/java/heroku_test/heroku_test/project/api/dto/ProjectDTO.java
+++ src/main/java/heroku_test/heroku_test/project/api/dto/ProjectDTO.java
@@ -2,10 +2,12 @@ package heroku_test.heroku_test.project.api.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Date;
 
+@NoArgsConstructor
 @Getter
 @Builder
 public class ProjectDTO {
