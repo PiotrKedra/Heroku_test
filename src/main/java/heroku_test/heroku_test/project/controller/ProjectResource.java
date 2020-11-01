da72b027713bbd745d6265674f9a9d915ce530ca
piotrkedra <piotr.k9750@gmail.com>
2020-11-01 17:59:16 +0100

    multipart test

--- src/main/java/heroku_test/heroku_test/project/controller/ProjectResource.java
+++ src/main/java/heroku_test/heroku_test/project/controller/ProjectResource.java
@@ -29,7 +29,7 @@ public class ProjectResource {
         return projectApi.test();
     }
 
-    @PostMapping(consumes = {"multipart/form-data"})
+    @PostMapping()
     @ResponseBody
     public Long create(@RequestPart("project") ProjectDTO project, @RequestPart("project_img")MultipartFile projectImg) {
         System.out.println(project);
